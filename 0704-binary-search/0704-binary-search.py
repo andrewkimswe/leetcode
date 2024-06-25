b598b35ae1344f17bfa8cd939c8409c11110f4f0
김지원 <110761377+andrewkimswe@users.noreply.github.com>
2024-06-25 15:18:24 +0900

    Time: 193 ms (43.01%), Space: 18.1 MB (69.30%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -3,7 +3,7 @@ class Solution:
         l, r = 0, len(nums) - 1
         
         while l <= r:
-            m = (l+r) // 2
+            m = l + ((r-l) // 2) # To prevent overflow
             if nums[m] > target:
                 r = m - 1
             elif nums[m] < target:
